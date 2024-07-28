6bbaeffec4acff78776de7bef40ec4a2d8e4e0f3
diudiu <hunter8vincent@gmail.com>
2024-07-28 12:43:58 +0800

    hello world

---
+++ build/src/main/CMakeFiles/live.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/live.dir/main.cpp.o"
+  "CMakeFiles/live.dir/main.cpp.o.d"
+  "live"
+  "live.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/live.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
